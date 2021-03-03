5f26b67713afdc401550902f695f4f9a3db2560f
beta <beta101093@mail.ru>
2021-03-03 21:43:22 +0300

    Написал переменные и вывел на экран. Задача 1

---
+++ ex_1.py
@@ -0,0 +1,6 @@
+name = 'Beidulla'
+age = 27
+print('My name is '+ name +'.\nI am ', age, ' years old')
+name = input('What is your name? - ')
+age = input('How old are you? - ')
+print('My name is '+ name +'.\nI am ', age, ' years old')
