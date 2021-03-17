4e0c43fb27fc0bdd17994624a900ccc4e5e4e5d2
beta <beta101093@mail.ru>
2021-03-18 01:20:28 +0300

    ex5_4 is done

---
+++ Lesson_5/ex5_4.py
@@ -0,0 +1,12 @@
+import json
+
+with open('ex5_4.json', 'r') as file:
+    line = dict(json.load(file))
+translate = ('odin', 'dva', 'tree', 'chetiri')
+num = 0
+with open('ex5_4_1.json', 'w+') as new_file:
+    for el, mel in line.items():
+        mel = translate[num]
+        print(f'{el} - {mel}')
+        new_file.write(f'{el} - {mel}\n')
+        num += 1
