b3e076e0acec266b40da96766dc4831f79936336
beta <beta101093@mail.ru>
2021-03-18 01:46:05 +0300

    ex5_4 is done with change

--- Lesson_5/ex5_4.py
+++ Lesson_5/ex5_4.py
@@ -6,7 +6,7 @@ translate = ('odin', 'dva', 'tree', 'chetiri')
 num = 0
 with open('ex5_4_1.json', 'w+') as new_file:
     for el, mel in line.items():
-        mel = translate[num]
+        el = translate[num]
         print(f'{el} - {mel}')
         new_file.write(f'{el} - {mel}\n')
         num += 1
